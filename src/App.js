1a4942afe9dbbca09091a212868adbc0e196972a
alinakaneeva <alinakaneeva@alinas-macbook.myfiosgateway.com>
2024-04-14 18:01:04 -0400

    works at all

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
+
+function App() {
+  return (
+    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      
+    );
+}
+
+export default App;
